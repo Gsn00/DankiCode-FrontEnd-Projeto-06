bc068425ea275ea0f6211a94b57b536424fee1e3
Gabriel <silvanovaesgabriel@gmail.com>
2022-10-26 17:30:49 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<title>Projeto 06</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<section class="main">
+		<div class="layer"></div>
+		<div class="container">
+			<header>
+				<div class="logo">Danki.Code</div>
+				<div class="main-items">
+					<div class="social-media">
+						<a href="https://facebook.com"><i class="fa-brands fa-facebook"></i></a>
+						<a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+					</div><!-- social-media -->
+					<nav class="desktop">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Sobre</a></li>
+							<li><a href="">Contato</a></li>
+						</ul>
+					</nav><!-- desktop -->
+					<nav class="mobile">
+						<i id="mobile-btn-bars" class="fa-solid fa-bars"></i>
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Sobre</a></li>
+							<li><a href="">Contato</a></li>
+						</ul>
+					</nav><!-- desktop -->
+				</div><!-- main-items -->
+			</header>
+
+			<div class="main-content">
+				<h1 class="title-header">Bem-vindo(a) ao nosso <span>Website</span>!</h1>
+				<p class="title-content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. Vivamus maximus et neque nec tincidunt. Vestibulum in felis dignissim, fermentum est vehicula, commodo diam. Duis quis faucibus purus. Nam id nisi at diam placerat eleifend sed volutpat mi. Phasellus mauris metus, efficitur et nibh ut, vestibulum accumsan nunc.
+				</p>
+				<p class="title-content">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. Vivamus maximus et neque nec tincidunt. Vestibulum in felis dignissim, fermentum est vehicula, commodo diam. Duis quis faucibus purus. Nam id nisi at diam placerat eleifend sed volutpat mi. Phasellus mauris metus, efficitur et nibh ut, vestibulum accumsan nunc.
+				</p>
+				<input type="button" value="Saiba mais!" class="btn">
+			</div><!-- main-content -->
+		</div><!-- container -->	
+	</section><!-- main -->
+
+	<section class="planos">
+		<div class="planos-header">
+			<div class="container">
+				<h2>CONFIRA NOSSOS PLANOS!</h2>
+			</div><!-- container -->
+		</div><!-- planos-header -->
+		<div class="container">
+			<section class="planos-vitrine">
+				
+				<div class="plano-single">
+					<h2>Mensal</h2>
+
+					<div class="plano-single-list">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+					</div><!-- plano-single-list -->
+
+					<h1>R$24,90</h1>
+				</div><!-- plano-single -->
+
+				<div class="plano-single">
+					<h2>Trimestral</h2>
+
+					<div class="plano-single-list">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+					</div><!-- plano-single-list -->
+
+					<h1>R$59,90</h1>
+				</div><!-- plano-single -->
+
+				<div class="plano-single">
+					<h2>Anual</h2>
+
+					<div class="plano-single-list">
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+						<p><i class="fa-solid fa-square-check"></i> Lorem ipsum silor domor.</p>
+					</div><!-- plano-single-list -->
+
+					<h1>R$199,90</h1>
+				</div><!-- plano-single -->
+
+			</section><!-- planos-vitrine -->
+		</div><!-- container -->
+	</section><!-- planos -->
+
+	<section class="parallax">
+		<div class="layer"></div>
+		<div class="container">
+			<h1 class="title-header">Sobre nossa <span>empresa</span></h1>
+			<p class="title-content">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. Vivamus maximus et neque nec tincidunt. Vestibulum in felis dignissim, fermentum est vehicula, commodo diam. Duis quis faucibus purus. Nam id nisi at diam placerat eleifend sed volutpat mi. Phasellus mauris metus, efficitur et nibh ut, vestibulum accumsan nunc.
+			</p>
+			<p class="title-content">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. Vivamus maximus et neque nec tincidunt. Vestibulum in felis dignissim, fermentum est vehicula, commodo diam. Duis quis faucibus purus. Nam id nisi at diam placerat eleifend sed volutpat mi. Phasellus mauris metus, efficitur et nibh ut, vestibulum accumsan nunc.
+			</p>
+		</div><!-- container -->
+	</section><!-- parallax -->
+
+	<section class="final-section">
+		<div class="container">
+			<div class="w50 contact">
+				<h1>Entre em contato</h1>
+				<form>
+					<input type="text" placeholder="Seu nome">
+					<input type="email" placeholder="Seu e-mail">
+					<textarea placeholder="Sua mensagem"></textarea>
+					<input type="button" value="Enviar" class="btn">
+				</form>
+			</div><!-- contact -->
+
+			<div class="w50 team">
+				<h1>Conheça nosso time!</h1>
+				<div class="team-member">
+					<div class="box-avatar"></div>
+					<div class="team-member-info">
+						<h4>Guilherme Grillo</h4>
+						<p>Instrutor / Founder</p>
+					</div><!-- team-member-info -->
+					<div class="team-member-text">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. 
+						</p>
+					</div><!-- team-member-text -->
+				</div><!-- team-member -->
+
+				<div class="team-member">
+					<div class="box-avatar"></div>
+					<div class="team-member-info">
+						<h4>John Green</h4>
+						<p>Administrador</p>
+					</div><!-- team-member-info -->
+					<div class="team-member-text">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar mi magna, a vestibulum leo luctus eu. Donec a ex sapien. Pellentesque at est nunc. Mauris semper vitae massa vel mollis. Vivamus pulvinar mi magna. Pellentesque at est nunc.
+						</p>
+					</div><!-- team-member-text -->
+				</div><!-- team-member -->
+
+			</div><!-- contact -->
+		</div><!-- container -->
+	</section><!-- final-section -->
+
+	<footer>
+		<div class="container">
+			<p>Todos os direitos reservados.</p>
+			<div class="social-media">
+				<a href="https://facebook.com"><i class="fa-brands fa-facebook"></i></a>
+				<a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+			</div><!-- social-media -->
+		</div><!-- container -->
+	</footer>
+
+	<!-- Script -->
+	<script src="js/all.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+	<script>
+		$(function() {
+			$('body').on('click', '#mobile-btn-bars', function() {
+				var el = $(this).parent().find('ul');
+				if ($(this).hasClass('fa-bars')) {
+					$(this).removeClass('fa-bars');
+					$(this).addClass('fa-xmark')
+				} else {
+					$(this).removeClass('fa-xmark');
+					$(this).addClass('fa-bars')
+				}
+				el.slideToggle();
+			});
+		});
+	</script>
+</body>
+</html>
